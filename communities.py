6cd83dc8f9540e1fd6523ddd4bab0b7d76837517
lkovanen <lkovanen>
2009-09-22 12:17:26 +0000

    Different classes for node partitions and covers. Also added a faster method for calculating the max variation of information.

--- communities.py
+++ communities.py
@@ -1,34 +1,43 @@
 import pynet,netext
-from math import log
 import numpy as np
 import info_theory as ith
+import operator
+#from logger import log
 
-#class CommStruct:
-class NodeFamily(object):
-    """
-    Defines a community structure of a network.
-    WARNING: Name of this class was changed
-
-    To be implemented:
-    *reading from string
-
-    """
+class NodeCover(object):
+    """Representation of possibly overlapping node partitions."""
     
-    #def __init__(self):
-    #    self.comm=[]
-
-    def __init__(self, cmap={}, inputFile=None):
-        self.comm=[]
+    def __init__(self, cmap={}, inputFile=None, N_nodes=None):
+        self._comm = []
         for community in cmap:
             self._addCommunity(cmap[community])
         if inputFile is not None:
             self._parseStrings(inputFile)
         self._sortBySize()
 
+        # Find out the number of nodes if not given.
+        if N_nodes is None:
+            all_nodes = set()
+            for c in self._comm:
+                all_nodes.update(c)
+            N_nodes = len(all_nodes)
+            del all_nodes
+        self.N_nodes = N_nodes
+
+    def _addCommunity(self,newCommunity):
+        self._comm.append(set(newCommunity))
+
     def _parseStrings(self,input):
         for line in input:
             fields = map(int, line.split())
             self._addCommunity(fields)
+
+    def _sortBySize(self):
+        self._comm.sort(key=len, reverse=True)     
+
+    @property
+    def comm(self):
+        return self._comm
 
     def __str__(self):
         string=""
@@ -37,13 +46,7 @@ class NodeFamily(object):
                 string=string+str(node)+" "
             string=string[0:len(string)-1]+"\n"
         return string
-
-    def _sortBySize(self):
-        self.comm.sort(key=len, reverse=True)     
-        
-    def _addCommunity(self,newCommunity):
-        self.comm.append(set(newCommunity))
-
+   
     def __getitem__(self,index):
         return self.comm[index]
 
@@ -135,8 +138,8 @@ class NodeFamily(object):
         """
 
         """
-        newcs = NodeFamily()
-        for community in self.comm:
+        newcs = NodeCover()
+        for community in self._comm:
             newCommunity = set()
             for oldnode in community:
                 for newnode in oldnode:
@@ -155,8 +158,8 @@ class NodeFamily(object):
         Returns new community structure based on this one and
         new nodes denoted by indices on this one
         """
-        newcs=NodeFamily({})
-        for community in self.comm:
+        newcs=NodeCover({})
+        for community in self._comm:
             newCommunity=set()
             for oldnode in community:
                 for newnode in newNodes[oldnode]:
@@ -170,65 +173,89 @@ class NodeFamily(object):
         newcs._sortBySize()
         return newcs
 
-
-    def getSetsForNodes(self):
+    def getMaxVariationOfInformation(self, otherCover):
+        """Return maximum variation of information.
         """
-        Returns a map of nodes to the set it belongs.
-        Sets are denoted by integers so that largest
-        community has smallest number etc.
-        """
-        nodeCommunity={}
-        for i,c in enumerate(self.comm):
-            for node in c:
-                nodeCommunity[node]=i
-        return nodeCommunity
-
-    def getEntropy(self):
-        """Calculate entropy.
-
-        Assumes that the sets in the family do not overlap.
-        """
-        len_c = map(len, self.comm)
-        return ith.entropy_X(np.array(len_c)/float(sum(len_c)))
-
-    def getMutualInformation(self,otherFamily):
-        i=0.0
-        #n_ij=[]
-        #n_i=map(len,list(self))
-        #n_j=map(len(list(otherFamily)))
-        n=float(sum(map(len,list(self))))
-        for c in self:
-            n_i=float(len(c))
-            for c2 in otherFamily:
-                n_j=float(len(c2))
-                n_ij=float(len(c.intersection(c2)))
-                if n_ij!=0:
-                    i+=n_ij/n*log(n_ij*n/n_i/n_j,2)
-        return i
-
-    def getNormalizedMutualInformation(self,otherFamily):
-        """Return normalized mutual information.
-
-        The normalized mutual information is
-             I_n(X;Y) = 2*I(X;Y)/(H(X)+H(Y))
-        I_n(X;Y) is always in [0, 1].
-        """
-        return (2*self.getMutualInformation(otherFamily)
-                /(self.getEntropy()+otherFamily.getEntropy()))
-
-    def getMImetric(self,otherFamily):
-        """Return a metric based on mutual information.
-
-        The returned value is
-             D(X,Y) = 1 - I(X;Y)/max{H(X), H(Y)}
-        D(X,Y) is a metric: it is symmetric, non-negative, satisfies
-        the triangle inequality and D(X,Y)=0 if and only if X==Y.
-        """
-        return 1.0 - (self.getMutualInformation(otherFamily)
-                      /max(self.getEntropy(),otherFamily.getEntropy()))
+        def p_log2_p(p):
+            return (0.0 if p == 0 else -p*np.log2(p))
         
-        
-    def getMaxVariationOfInformation(self, otherFamily, N_nodes=None):
+        def create_commIDs(comm):
+            """Construct commIDs from comm.
+
+            The key is node ID, and the value will be a list of
+            community IDs this node belongs to.
+            """
+            commIDs = {}
+            for c_num, c in enumerate(comm):
+                for node in c:
+                    commIDs.setdefault(node, []).append(c_num)
+            return commIDs
+
+        # Find out the number of nodes.
+        Nf = float(max(self.N_nodes, otherCover.N_nodes))
+
+        # Construct community ID dictionaries.
+        cID_A = create_commIDs(self.comm)
+        cID_B = create_commIDs(otherCover.comm)
+
+        # Construct a bipartite community net where nodes correspond
+        # to communities and edge weight is the number of common nodes
+        # between the two communities.
+        Nc_A = len(self.comm)
+        Nc_B = len(otherCover.comm)
+        commNet = pynet.SymmNet()
+        for node in xrange(int(Nf)):
+            try:
+                for cj in cID_B[node]:
+                    cj += Nc_A
+                    for ci in cID_A[node]:
+                        commNet[ci, cj] += 1
+            except AttributeError:
+                # `node` does not occur in one of the node
+                # covers. Skip this node.
+                continue
+
+        # List of community sizes.
+        comm_sizes = map(len, self.comm) + map(len, otherCover.comm)
+
+        ret_val = 1.0
+        for IX, IY in [(xrange(Nc_A), xrange(Nc_A, Nc_A+Nc_B)),
+                       (xrange(Nc_A, Nc_A+Nc_B), xrange(Nc_A))]:
+            # IX contains the IDs of first communities (first node
+            # cover), IY the IDs of the second communities. These are
+            # flipped on the second iteration to calculate the same
+            # thing the other way around.
+            H_norm = []
+            for X_k in IX:
+                # Calculate the entropies H(X_k|Y_l) for all Y_l in IY
+                # and find the minimum. H_min is initialized to
+                # H(X_k), which is the maximum value and will be the
+                # final value if no Y_l is accepted.
+                px = comm_sizes[X_k]/Nf
+                H_min = H_X = ith.entropy_X([px, 1-px])
+                for Y_l in commNet[X_k]:
+                    cut_size = commNet[X_k][Y_l]
+                    union_size = comm_sizes[X_k] + comm_sizes[Y_l] - cut_size
+                    hP_same = (p_log2_p(cut_size/Nf)
+                               + p_log2_p((Nf - union_size)/Nf))
+                    hP_diff = (p_log2_p((comm_sizes[X_k] - cut_size)/Nf)
+                               + p_log2_p((comm_sizes[Y_l] - cut_size)/Nf))
+                    if (hP_same > hP_diff):
+                        py = comm_sizes[Y_l]/Nf
+                        H_Y = ith.entropy_X([py, 1-py])
+                        H_min = min(H_min, hP_same + hP_diff - H_Y)
+
+                        #print (X_k, Y_l, hP_same, hP_diff, hP_same > hP_diff,
+                        #       (hP_same + hP_diff - H_Y)/H_X)
+                H_norm.append((0 if H_X == 0 else H_min/H_X))
+
+            ret_val -= 0.5*sum(H_norm)/len(IX)
+            #print H_norm, sum(H_norm)/len(IX)
+
+        return ret_val
+
+
+    def getMaxVariationOfInformation_slow(self, otherCover, N_nodes=None):
         """Return maximum variation of information.
 
         The maximum variation of information can be used to compare
@@ -241,7 +268,7 @@ class NodeFamily(object):
 
         Parameters
         ----------
-        otherFamily : NodeFamily object
+        otherCover : NodeCover object
            The other community sructure to compare with.
         N_nodes : int
            The total number of nodes in all communities. If None, the
@@ -252,11 +279,15 @@ class NodeFamily(object):
 
         Return
         ------
-        N : float
+        mv : float
            The maximum variation of information.
 
         Notes
         -----
+        Time complexity is O(N*M), where N is the number of
+        communities in `self` and M is the number of communities in
+        `otherCover`.
+
         If one community structure consists of only one community
         covering all nodes, this measure is always 0.5 (unless of
         course the other one is identical, in which case 1.0 is
@@ -269,36 +300,189 @@ class NodeFamily(object):
         # Find out the number of nodes if not given. `Nf` is defined
         # just to make the code more readable.
         if N_nodes is None:
-            N_nodes = len(set(reduce(set.union, self.comm+otherFamily.comm)))
+            all_nodes = set()
+            for c in self.comm:
+                all_nodes.update(c)
+            N_nodes = len(all_nodes)
+            del all_nodes
         Nf = float(N_nodes)
 
         ret_val = 1.0
-        for XF, YF in [(self, otherFamily), (otherFamily, self)]:
+        for XF, YF in [(self.comm, otherCover.comm),
+                       (otherCover.comm, self.comm)]:
             H_norm = []
             for X_k in XF:
                 # Calculate the entropies H(X_k|Y_l) for all Y_l in YF
-                # and find the minimum. H_min is initialized to H(X_k).
+                # and find the minimum. H_min is initialized to
+                # H(X_k), which is the maximum value and will be the
+                # final value if no Y_l is accepted.
                 px = len(X_k)/Nf
                 H_min = H_X = ith.entropy_X([px, 1-px])
                 for Y_l in YF:
-                    py = len(Y_l)/Nf
-                    H_Y = ith.entropy_X([py, 1-py])
                     cut_size = len(X_k.intersection(Y_l))
                     hP_same = (p_log2_p(cut_size/Nf)
                                + p_log2_p((Nf - len(X_k.union(Y_l)))/Nf))
                     hP_diff = (p_log2_p((len(X_k) - cut_size)/Nf)
                                + p_log2_p((len(Y_l) - cut_size)/Nf))
                     if (hP_same > hP_diff):
+                        py = len(Y_l)/Nf
+                        H_Y = ith.entropy_X([py, 1-py])
                         H_min = min(H_min, hP_same + hP_diff - H_Y)
 
-                    #print (X_k, Y_l, hP_same, hP_diff, hP_same > hP_diff,
-                    #       (hP_same + hP_diff - H_Y)/H_X)
+                        #print (X_k, Y_l, hP_same, hP_diff, hP_same > hP_diff,
+                        #       (hP_same + hP_diff - H_Y)/H_X)
                 H_norm.append((0 if H_X == 0 else H_min/H_X))
 
             ret_val -= 0.5*sum(H_norm)/len(XF)
             #print H_norm, sum(H_norm)/len(XF)
 
         return ret_val
+
+
+class NodePartition(NodeCover):
+    """Representation of node partitions.
+
+    Each node must belong to exactly one partition.  Node names must
+    be integers from 0 to N-1, where N is the number of nodes in the
+    network.
+    """
+
+    def __init__(self, cmap={}, inputFile=None, N_nodes=None):
+        # Create a list [(comm_size, comm_ID), ...] and sort it in
+        # decreasing order.
+        comm_sizes = [(len(comms), commID) for commID, comms in cmap.iteritems()]
+        comm_sizes.sort()
+        comm_sizes.reverse()
+
+        # Construct a dictionary {node_ID: comm_ID} so that comm_ID is
+        # order by community size.
+        self._commIDs = {} # Dictionary of community IDs of each node.
+        for newCommID, (s, oldCommID) in enumerate(comm_sizes):
+            for node in cmap[oldCommID]:
+                self._commIDs[node] = newCommID
+        self.N_communities = len(cmap) + 1
+
+        # List of community sizes.
+        self.C_sizes = map(operator.itemgetter(0), comm_sizes)
+
+        # If `N_nodes` is not given, use the number of nodes just read.
+        if N_nodes is None:
+            N_nodes = len(self._commIDs)
+        self.N_nodes = N_nodes
+
+        # Save calculated mutual informations with other nodeFamilies
+        # into `self.MIs`. This way mutual information is not calculated
+        # again if for example getNormalizedMutualInformation is
+        # called after calling getMutualInformation.
+        self.MIs = {} 
+
+    @property
+    def comm(self):
+        """List of community sets.
+
+        Uses the same community indices as self._commIDs. Because the
+        original community indices have been chosen based on the
+        community size, there is no need to sort self._comm again.
+        """
+        try:
+            return self._comm
+        except AttributeError:
+            self._comm = [set() for i in range(self.N_communities)]
+            for node, commID in self._commIDs.iteritems():
+                self._comm[commID].add(node)
+            return self._comm
+
+    def getSetsForNodes(self):
+        """Return a map of nodes to the set it belongs."""
+        return self.commIDs
+
+    @property
+    def entropy(self):
+        """Entropy of the node partition."""
+        try:
+            return self._entropy
+        except AttributeError:
+            len_c = self.C_sizes
+            self._entropy = ith.entropy_X(np.array(len_c)/float(sum(len_c)))
+            return self._entropy
+
+    def getMutualInformation_slow(self, otherPartition):
+        """Calculate mutual information.
+
+        Note that the time complexity of this algorithm is O(N*M),
+        where N is the number of communities in self and M is the
+        number of communities in `otherPartition`.
+        """
+        mi=0.0
+        n=float(sum(map(len,list(self))))
+        for c in self:
+            n_i=float(len(c))
+            for c2 in otherPartition:
+                n_j=float(len(c2))
+                n_ij=float(len(c.intersection(c2)))
+                if n_ij!=0:
+                    mi += n_ij/n*np.log2(n_ij*n/n_i/n_j)
+        return mi
+
+    def getMutualInformation(self, otherPartition):
+        """Calculate mutual information.
+
+        Time complexity is O(N+E), where N is the number of nodes
+        and E is the number of overlapping pairs of communities.
+        """
+        # Check if mutual information has already been calculated.
+        if id(otherPartition) in self.MIs:
+            return self.MIs[id(otherPartition)]
+        
+        # Construct a bipartite community net where nodes correspond
+        # to communities and edge weight is the number of common nodes
+        # between the two communities.
+        commNet = pynet.SymmNet()
+        for node in self._commIDs:
+            ci = self._commIDs[node]
+            cj = self.N_communities + otherPartition._commIDs[node]
+            commNet[ci, cj] += 1
+
+        # Calculate mutual information.
+        mi=0.0
+        n = self.N_nodes
+        for ci, cj, n_ij in commNet.edges:
+            ci, cj = min(ci, cj), max(ci, cj)-self.N_communities
+            n_i = self.C_sizes[ci]
+            n_j = otherPartition.C_sizes[cj]
+            mi += n_ij/n*np.log2(n_ij*n/n_i/n_j)
+
+        # Add this mutual information to the list of already
+        # calculated mutual informations.
+        self.MIs[id(otherPartition)] = mi
+        otherPartition.MIs[id(self)] = mi
+
+        return mi
+
+    def getNormalizedMutualInformation(self,otherPartition):
+        """Return normalized mutual information.
+
+        The normalized mutual information is
+             I_n(X;Y) = 2*I(X;Y)/(H(X)+H(Y))
+        I_n(X;Y) is always in [0, 1].
+        """
+        return (2*self.getMutualInformation(otherPartition)
+                /(self.entropy + otherPartition.entropy))
+
+    def getMImetric(self,otherPartition):
+        """Return a metric based on mutual information.
+
+        The returned value is
+             D(X,Y) = 1 - I(X;Y)/max{H(X), H(Y)}
+        D(X,Y) is a metric: it is symmetric, non-negative, satisfies
+        the triangle inequality and D(X,Y)=0 if and only if X==Y.
+        """
+        return 1.0 - (self.getMutualInformation(otherPartition)
+                      /max(self.entropy, otherPartition.entropy))
+        
+    def getMaxVariationOfInformation(self, otherCover, N_nodes=None):
+        return super(NodePartition, self).getMaxVariationOfInformation(
+            otherCover, len(self._commIDs))
 
 class communityTree:
     """
@@ -315,7 +499,7 @@ class communityTree:
         self.tree.append([])
 
         self.net=pynet.SymmNet()
-        #self.multiplier=10**ceil(log(len(cslist))/log(10))
+        #self.multiplier=10**ceil(np.log10(len(cslist)))
         
         #add roots:
         for c in cslist[0]:
