7f1e3a93e53f4ade4a05440f3f0c795d7bc818b0
mtkivela <mtkivela>
2010-02-01 17:18:53 +0000

    Changes to the properties caused by the version change of pynet.py from 1.3 to 1.4, and new tests.

--- tests/test_pynet.py
+++ tests/test_pynet.py
@@ -3,27 +3,70 @@ from operator import itemgetter
 from netpython import pynet
 
 
+
+
 class TestPynet(unittest.TestCase):
     
     def setUp(self):
         pass
                 
-    def test_basic_Net_construction(self):
-        myNet=pynet.Net()
+    def test_basic_dir(self,netType):
+        myNet=netType(sizeLimit=10)
+
         myNet[1,2]=10.0
         self.assertEqual(myNet[1,2],10.0)
         self.assertEqual(myNet[2,1],0.0)
 
+    def test_basic_symm(self,netType):
+        myNet=netType(sizeLimit=10)
 
+        myNet[1,2]=10.0
+        self.assertEqual(myNet[1,2],10.0)
+        self.assertEqual(myNet[2,1],10.0)
         
+        myNet[1,"foo"]=0.1
+        self.assertEqual(myNet[1].deg(),2)
+        self.assertEqual(myNet.deg(1),2)
+
+        del myNet["foo"]
+        self.assertEqual(myNet[1].deg(),1)
+        self.assertEqual(list(myNet[1]),[2])
+        
+        self.assertEqual(len(myNet),2)
+        myNet.addNode("bar")
+        self.assertEqual(len(myNet),3)
+        self.assertEqual(myNet["bar"].deg(),0)
+
+
+    def test_basic_dir_ScipySparseDirNet(self):
+        self.test_basic_dir(pynet.ScipySparseDirNet)
+
+    def test_basic_dir_NumpyFullDirNet(self):
+        self.test_basic_dir(pynet.NumpyFullDirNet)
+
+    def test_basic_symm_ScipySparseSymmNet(self):
+        self.test_basic_symm(pynet.ScipySparseSymmNet)
+
+    def test_basic_symm_NumpyFullSymmNet(self):
+        self.test_basic_symm(pynet.NumpyFullSymmNet)
+
+    def test_basic_symm_LCELibSparseSymmNet(self):
+        self.test_basic_symm(pynet.LCELibSparseSymmNet)
+
+
+def test_pynet():
+    suite = unittest.TestSuite()    
+    #symmetric tests:
+    suite.addTest(TestPynet("test_basic_symm_ScipySparseSymmNet"))
+    suite.addTest(TestPynet("test_basic_symm_NumpyFullSymmNet"))
+    suite.addTest(TestPynet("test_basic_symm_LCELibSparseSymmNet"))
+
+    #dir tests:
+    suite.addTest(TestPynet("test_basic_dir_ScipySparseDirNet"))
+    suite.addTest(TestPynet("test_basic_dir_NumpyFullDirNet"))
+
+    unittest.TextTestRunner().run(suite)    
 
 if __name__ == '__main__':
-    if True:
-        # If true, run only the tests listed below, otherwise run all tests
-        # (this option is for testing the tests :-) )
-        suite = unittest.TestSuite()
-        suite.addTest(TestPynet("test_basic_Net_construction"))
-        unittest.TextTestRunner().run(suite)
-    else:
-        # Run all tests.
-        unittest.main()
+    runTests()
+
