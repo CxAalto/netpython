32ce72e1884d75cad403341a08894528e6360bcc
mtkivela <mtkivela>
2010-02-01 17:17:23 +0000

    A complete rewrite of the module. Some minor changes to the interface (mostly adding and removing nodes)

--- pynet.py
+++ pynet.py
@@ -1,187 +1,407 @@
 import numpy
+import scipy.sparse
+import itertools
 
-class Net(object):
-	"""A sparse matrix"""
 
-	def __init__(self):
+class VirtualNet(object):
+	"""
+	Virtual network class. Node names are sparse in the
+	interface, but dense in the backend.
+	Backend must implement following functions:
+	...
+	"""
+	def __init__(self,sizeLimit=0):
+		self._removedNodes=[]
 		self._nodes={}
-		self.type="py"
-
-	def _legaledge(self, src, dst):
-		# Override this for symmetrical/two-way linked ones
-		return True
-
-	def _intsetedge(self, src, dst, val):
-		# Override this for symmetrical/two-way linked ones
-		if val==0:
-			if src in self._nodes:
-				del self._nodes[src][dst]
-				if len(self._nodes[src])==0:
-					#delete isolated node
-					del self._nodes[src]
-		else:
-			if not src in self._nodes:
-				self._nodes[src]={dst: val}
-			else:
-				self._nodes[src][dst]=val
-	
-	def _setedge(self, src, dst, val):
-		assert self._legaledge(src, dst)
-		self._intsetedge(src, dst, val)
-		assert self._legaledge(src, dst)
-
-	def __getitem__(self, args):
+		self._indexToName=[]
+		self.sizeLimit=sizeLimit
+	def __getitem__(self,args):
 		if isinstance(args, tuple):
 			if len(args) != 2:
 				raise KeyError, "Don't be silly. One or two indices"
-			assert self._legaledge(args[0],args[1])
 			try:
-				retval=self._nodes[args[0]][args[1]]
+				n1=self._nodes[args[0]]
 			except KeyError:
-				return 0
-			return retval
+				return 0.0
+				#raise KeyError, "No such node: "+str(args[0])
+			try:
+				n2=self._nodes[args[1]]
+			except KeyError:
+				return 0.0
+				#raise KeyError, "No such node: "+str(args[1])
+			assert n1!=n2, "No self-edges."
+			return self._getEdge(n1,n2)
 		else:
-			return Node(self, args)
-		
-	def __setitem__(self, key, val):
+			return Node(self,args)
+	def __setitem__(self,key,val):
 		if isinstance(key, tuple):
 			if len(key) != 2:
-				raise KeyError, "Don't be silly. One or two args"
-			self._setedge(key[0], key[1], val) 
-			return val
-		else:
-			if isinstance(val, Node):
-				if val.index in val.net._nodes:
-					val=val.net._nodes[val.index]
-				else:
-					val={}
-			if not isinstance(val, dict):
-				raise NotImplemented, \
-						"Setting nodes only implemented from maps"
-			del self[key] # calls __delitem__
-			# We perform a deep copy
-			for edge in val.iteritems():
-				self._setedge(key, edge[0], edge[1])
-			return Node(self, key)
-	
-	def __delitem__(self, args):
+				raise KeyError, "Don't be silly. Give two indices"
+		if key[0] not in self:
+			self.addNode(key[0])
+		if key[1] not in self:
+			self.addNode(key[1])
+		assert key[0]!=key[1], "No self-edges."		
+		self._setEdge(self._nodes[key[0]],self._nodes[key[1]],val)
+		return val
+
+	def __delitem__(self,args):
 		if isinstance(args, tuple):
 			if len(args) != 2:
 				raise KeyError, "Don't be silly. One or two indices"
-			self._setedge(args[0], args[1], 0)
+			self[args[0], args[1]]=0
 		else:
-			for dest in self[args]:
-				# self[args] returns a node, for which .iter()
-				self[args,dest]=0
+			self.delNode(args)
 
+	def __contains__(self,nodeName):
+		return nodeName in self._nodes
 	def __iter__(self):
-		return self._nodes.keys().__iter__()
-
-	def isSymmetric(self):
-		return False
-
+		return self._nodes.iterkeys()
 	def __len__(self):
 		return len(self._nodes)
+	def addNode(self,nodeName):
+		"""
+		Adds an empty node to the network.
+		"""
+		if len(self._removedNodes)>0:
+			newIndex=self._removedNodes.pop()
+			self._indexToName[newIndex]=nodeName
+		else:
+			newIndex=len(self._nodes)
+			#check for size limit
+			if self.sizeLimit!=0 and newIndex>self.sizeLimit:
+				raise Exception,"Size limit for the network reached." 
+			self._indexToName.append(nodeName)
+		self._nodes[nodeName]=newIndex
+
+	def delNode(self,nodeName): #override for directed
+		"""
+		Removes a node from the network.	     
+		"""
+		#Check that node is in the net
+		if nodeName in self:
+		        #First, remove all the edges
+			self._removeNodeEdges(nodeName)
+			
+			#then remove the empty node
+			removedIndex=self._nodes[nodeName]
+			self._removedNodes.append(removedIndex)
+			del self._nodes[nodeName]
+
+		
+	       
+	def isSymmetric(self):
+		return self.symmetric
+
+	def deg(self,nodeName):
+		return self._degIndex(self._nodes[nodeName])
+
+	def inDeg(self,nodeName):
+		return self._inDegIndex(self._nodes[nodeName])
+
+	def outDeg(self,nodeName):
+		return self._outDegIndex(self._nodes[nodeName])
+
+	#--- Symmetric net:
+	def _inDegIndex(self,nodeIndex):
+		return self._degIndex(nodeIndex)
+	def _outDegIndex(self,nodeIndex):
+		return self._degIndex(nodeIndex)
+	def _iterNodeIn(self,nodeIndex):
+		return self._iterNode(nodeIndex)
+	def _iterNodeOut(self,nodeIndex):
+		return self._iterNode(nodeIndex)
+	def _removeNodeEdges(self,nodeName):
+		for neigh in list(self[nodeName]):
+			self[nodeName,neigh]=0
+	symmetric=True
+
+	#--- Virtual functions to override
+	def _degIndex(self,nodeIndex):
+		raise NotImplemented
+	def _getEdge(self,src,dest):
+		raise NotImplemented
+	def _setEdge(self,src,dest,val):
+		raise NotImplemented
+	def _iterNode(self,nodeIndex):
+		raise NotImplemented
+	
 
 class Node(object):
-	def __init__(self, net, index):
-		self.net=net;
-		self.index=index;
-
+	def __init__(self,net,name):
+		self.net=net
+		self.name=name
+	def _indexToNameIter(self,iterator):
+		for index in iterator:
+			yield self.net._indexToName[index]
 	def __iter__(self):
-		if not self.index in self.net._nodes:
-			return iter([])
-		return self.net._nodes[self.index].keys().__iter__()
-		#return self.net._nodes[self.index].__iter__()
+		if not self.name in self.net:
+			return [].__iter__()
+			#raise KeyError, "Node not in the network."
+		#name -> index -> backend iterator -> index -> name
+		return self._indexToNameIter(self.net._iterNode(self.net._nodes[self.name]))
 
-	def __getitem__(self, index):
-		return self.net[self.index, index]
+	def __getitem__(self, name):
+		return self.net[self.name, name]
 
-	def __setitem__(self, index, val):
-		 self.net[self.index, index]=val
+	def __setitem__(self, name, val):
+		 self.net[self.name, name]=val
 		 return val
 
 	def deg(self):
-		return len(self.net._nodes[self.index])
+		return self.net.deg(self.name)
 
-class SymmNet(Net):
-	"""A net with forced symmetry"""
-	def _legaledge(self, src, dst):
-		if src in self._nodes:
-			if dst in self._nodes[src]:
-				return dst in self._nodes \
-						and src in self._nodes[dst] \
-						and self._nodes[src][dst]==self._nodes[dst][src]
-		#either no src or no edge src->dst
-		return dst not in self._nodes or src not in self._nodes[dst]
+	def inDeg(self):
+		return self.net.inDeg(self.name)
 
-	def _intsetedge(self, src, dst, val):
-		Net._intsetedge(self, src, dst, val)
-		Net._intsetedge(self, dst, src, val)
-		return val
-				
-	def isSymmetric(self):
-		return True
-			
-class FullNet(Net):
-	def __init__(self,size):
-		super(Net,self)
-		self.size=size
-		#self._nodes=FullNet.Nodes(size)
-		self._nodes=numpy.zeros([size,size])
-		
-	def _intsetedge(self, src, dst, val):				
-		self._nodes[src][dst]=val		
+	def outDeg(self):
+		return self.net.outDeg(self.name)
 
-	def __getitem__(self,args):
-		if isinstance(args,tuple):
-			return self._nodes[args[0],args[1]]
+
+	def iterIn(self):
+		if not self.name in self.net:
+			raise KeyError, "Node not in the network."
+		#name -> index -> backend iterator -> index -> name
+		return self._indexToNameIter(self.net._iterNodeIn(self.net._nodes[name]))
+	def iterOut(self):
+		if not self.name in self.net:
+			raise KeyError, "Node not in the network."
+		#name -> index -> backend iterator -> index -> name
+		return self._indexToNameIter(self.net._iterNodeOut(self.net._nodes[name]))
+	
+
+class VirtualDirNet(VirtualNet):
+	def _removeNodeEdges(self,nodeName):
+		for neigh in self[nodeName]:
+			self[node,neigh]=0
+			self[neigh,node]=0
+	symmetric=False
+
+	#--- Virtual functions to override
+	def _iterNodeIn(self,nodeIndex):
+		raise NotImplemented
+	def _iterNodeOut(self,nodeIndex):
+		raise NotImplemented
+	def _inDegIndex(self,nodeIndex):
+		raise NotImplemented
+	def _outDegIndex(self,nodeIndex):
+		raise NotImplemented
+
+class ScipySparseSymmNet(VirtualNet):
+	def __init__(self,sizeLimit=0):
+		VirtualNet.__init__(self,sizeLimit=sizeLimit)
+		self._nodeList=[]
+	def _addNode(self):
+		newNode=scipy.sparse.dok_matrix((10**10,1),dtype='float')
+		self._nodeList.append(newNode)
+
+	#--- Methods used by VirtualNet:
+	def _degIndex(self,nodeIndex):
+		return len(self._nodeList[nodeIndex])
+	def _getEdge(self,src,dest):
+		return self._nodeList[src][dest,0]
+	def _setEdge(self,src,dest,val):
+		nNodes=len(self._nodeList)
+		if src>=nNodes: 
+			self._addNode()
+		if dest>=nNodes: 
+			self._addNode()
+		if val==0:
+			try:
+				del self._nodeList[src][dest,0]
+			except KeyError:
+				pass
+			try:
+				del self._nodeList[dest][src,0]
+			except KeyError:
+				pass
 		else:
-			return FullNode(self,args)						
-			
-	def __iter__(self):
-		return range(0,self.size).__iter__()
+			self._nodeList[src][dest,0]=val
+			self._nodeList[dest][src,0]=val
+	def _iterNode(self,nodeIndex):
+		return itertools.imap(lambda x:x[0],self._nodeList[nodeIndex].iterkeys())
 
-	def __len__(self):
-		return self.size
 
-	def getWeightMatrix(self):
-		return self._nodes
+class ScipySparseDirNet(VirtualDirNet):
+	def __init__(self,sizeLimit=0):
+		VirtualNet.__init__(self,sizeLimit=sizeLimit)
+		self._nodeList=[] #directed edges and weights
+		self._backNodeList=[] #reversed directed edges 
+		self._totalDeg=[] #total degree of nodes
+	def _addNode(self):
+		newNode=scipy.sparse.dok_matrix((10**10,1),dtype='float')
+		self._nodeList.append(newNode)
+		newBackNode=scipy.sparse.dok_matrix((10**10,1),dtype='bool')
+		self._backNodeList.append(newBackNode)
+		self._totalDeg.append(0)
+
+	#--- Methods used by VirtualDirNet:
+	def _degIndex(self,nodeIndex):
+		return self.totalDeg(nodeIndex)
+	def _getEdge(self,src,dest):
+		return self._nodeList[src][dest,0]
+	def _setEdge(self,src,dest,val):
+		nNodes=len(self._nodeList)
+		if src>=nNodes: 
+			self._addNode()
+		if dest>=nNodes: 
+			self._addNode()
+		if val==0: #removing an edge
+			#nothing happens if the link does not exist:
+			if (dest,0) in self._nodeList[src]:
+				if not (src,0) in self._nodeList[dest]:
+					self._totalDeg[src]+=-1
+					self._totalDeg[dest]+=-1
+				self._nodeList[src][dest,0]=0
+				self._backNodeList[dest][src,0]=0
+		else:
+			if not (dest,0) in self._nodeList[src] and not (src,0) in self._nodeList[dest]:
+				self._totalDeg[src]+=1
+				self._totalDeg[dest]+=1
+
+			self._nodeList[src][dest,0]=val
+			self._backNodeList[dest][src,0]=1
+
+
+	def _iterNode(self,nodeIndex):
+		#First iter through all outgoing neighbors:
+		for neigh in self._nodeList[nodeIndex]:
+			yield neigh
+		#Then iter through only incoming neighbors
+		for neigh in self._backNodeList[nodeIndex]:
+			if not (neigh,0) in self._nodeList[nodeIndex]:
+				yield neigh
+	def _iterNodeIn(self,nodeIndex):
+		return itertools.imap(lambda x:x[0],self._backNodeList[nodeIndex].iterkeys())
+	def _iterNodeOut(self,nodeIndex):
+		return itertools.imap(lambda x:x[0],self._nodeList[nodeIndex].iterkeys())
+
+	def _inDegIndex(self,nodeIndex):
+		return len(self._backNodeList[nodeIndex])
+	def _outDegIndex(self,nodeIndex):
+		return len(self._nodeList[nodeIndex])
+
+class NumpyFullSymmNet(VirtualNet):
+	def __init__(self,sizeLimit):
+		VirtualNet.__init__(self,sizeLimit=sizeLimit)
+		self._adjMatrix=numpy.zeros([sizeLimit,sizeLimit])
+		self._degree=numpy.zeros([self.sizeLimit,1])
+
+	#--- Methods used by VirtualNet:
+	def _degIndex(self,nodeIndex):
+		return self._degree[nodeIndex]
+	def _getEdge(self,src,dest):
+		return self._adjMatrix[src,dest]
+	def _setEdge(self,src,dest,val):
+		if val==0 and self._adjMatrix[src,dest]!=0:
+			self._degree[src]+=-1
+			self._degree[dest]+=-1
+		elif val!=0 and self._adjMatrix[src,dest]==0:
+			self._degree[src]+=1
+			self._degree[dest]+=1
+		self._adjMatrix[src][dest]=val
+		self._adjMatrix[dest][src]=val
+	def _iterNode(self,nodeIndex):
+		for i in range(0,self.sizeLimit):
+                        if self._adjMatrix[nodeIndex,i]!=0:
+                                yield i
+
+class NumpyFullDirNet(VirtualDirNet):
+	def __init__(self,sizeLimit):
+		VirtualNet.__init__(self,sizeLimit=sizeLimit)
+		self._adjMatrix=numpy.zeros([sizeLimit,sizeLimit])
+		self._degree=numpy.zeros([self.sizeLimit,1])
+		self._inDegree=numpy.zeros([self.sizeLimit,1])
+		self._outDegree=numpy.zeros([self.sizeLimit,1])
+
+	#--- Methods used by VirtualDirNet:
+	def _degIndex(self,nodeIndex):
+		return self._degree[nodeIndex]
+	def _getEdge(self,src,dest):
+		return self._adjMatrix[src,dest]
+	def _setEdge(self,src,dest,val):
+		if val==0:
+			if self._adjMatrix[src,dest]!=0:
+				self._outDegree[src]+=-1
+				self._inDegree[dest]+=-1
+				if self._adjMatrix[dest,src]==0:
+					self._degree[src]+=-1
+					self._degree[dest]+=-1
+		elif val!=0:
+			if self._adjMatrix[src,dest]==0:
+				self._outDegree[src]+=1
+				self._inDegree[dest]+=1
+				if self._adjMatrix[dest,src]==0:
+					self._degree[src]+=1
+					self._degree[dest]+=1
+		self._adjMatrix[src][dest]=val
+	def _iterNode(self,nodeIndex):
+		for i in range(0,self.sizeLimit):
+                        if self._adjMatrix[nodeIndex,i]!=0 or self._adjMatrix[i,nodeIndex]!=0:
+                                yield i
+	def _iterNodeIn(self,nodeIndex):
+		for i in range(0,self.sizeLimit):
+                        if self._adjMatrix[i,nodeIndex]!=0:
+                                yield i
+	def _iterNodeOut(self,nodeIndex):
+		for i in range(0,self.sizeLimit):
+                        if self._adjMatrix[nodeIndex,i]!=0:
+                                yield i
+	def _inDegIndex(self,nodeIndex):
+		return self._inDegree[nodeIndex]
+	def _outDegIndex(self,nodeIndex):
+		return self._outDegree[nodeIndex]
+
+
+
+class LCELibSparseSymmNet(VirtualNet):
+	def __init__(self,sizeLimit=0):
+		VirtualNet.__init__(self,sizeLimit=sizeLimit)
+		self._net=_cnet.new_Sn(0)
+	def __del__(self):
+		_cnet.delete_Sn(self._net)
 		
-class FullNode(Node):
-	def __iter__(self):
-		for i in range(0,self.net.size):
-			if self.net[self.index,i]!=0:
-				yield i
 
-	def deg(self):
-		#this is slow, maybe you should have these in memory
-		return len(list(self))
+	#--- Methods used by VirtualNet:
+	def _degIndex(self,nodeIndex):
+		return _cnet.Sn_getDegree(self._net,nodeIndex)
+	def _getEdge(self,src,dest):
+		return _cnet.Sn_getEdge(self._net,src,dest)
+	def _setEdge(self,src,dest,val):
+		_cnet.Sn_setEdge(self._net,src,dest, val)
+	def _iterNode(self,nodeIndex):
+		citerator=_cnet.Sn_getNeighborIterator(self._net,nodeIndex)
+		next=_cnet.NeighborIterator_getNext(citerator)
+		while next!=-1:
+			yield next
+			next=_cnet.NeighborIterator_getNext(citerator)
+		_cnet.delete_NeighborIterator(citerator)
 
-class SymmFullNet(FullNet):
-	def _intsetedge(self, src, dst, val):				
-		self._nodes[src][dst]=val		
-		self._nodes[dst][src]=val		
+#--- Implementation lists
+SymmBackends=[ScipySparseSymmNet,NumpyFullSymmNet]
+DirBackends=[ScipySparseDirNet,NumpyFullDirNet]
 
-	def isSymmetric(self):
-		return True
+#--- Default implementations
+DirNet=ScipySparseDirNet 
+Net=DirNet
+SymmNet=ScipySparseSymmNet #LCELibSparseSymmNet overrides this
+SymmFullNet=NumpyFullSymmNet
+FullNet=NumpyFullDirNet
 
 
-
+#--- Try to import the C++-implementation.
 try:
-	from cnet import pynet_c
-	#from cnet.pynet_c import SymmNet as SymmNet_c	
-	class SymmNet(pynet_c.SymmNet_c,Net):
-		pass
-	class Node_(pynet_c.Node_c,Node):
-		pass
-	pynet_c.Node_c=Node_
-
+        from cnet import _cnet
+	SymmNet=LCELibSparseSymmNet
+	SymmBackends.append(LCELibSparseSymmNet)
 except ImportError:
-	print "Loading C interface failed."
+        print "Importing the LCELib network data structure failed."
+
+
+
 
 
 if __name__ == '__main__':
     """Run unit tests if called."""
     from tests.test_pynet import *
-    unittest.main()
+    test_pynet()
