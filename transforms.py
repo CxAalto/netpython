9288778461d07049b84b1d3c3cef07e26d55d738
lkovanen <lkovanen>
2010-02-10 12:57:03 +0000

    Cleaned up a bit.

--- transforms.py
+++ transforms.py
@@ -1,6 +1,6 @@
 import pynet,netext,percolator
 import random
-
+import numpy as np
 
 def mst(net,maximum=False):
     """Find a minimum/maximum spanning tree
@@ -264,74 +264,172 @@ def local_threshold_by_value(net,threshold):
 
     return newnet
 
-		
+def getLineGraph(net, useWeights=False, output=None, format='edg'):
+    """Return a line graph constructed from `net`.
+
+    The nodes in the line graph correspond to edges in the original
+    graph, and there is an edge between two nodes if they have a
+    common incident node in the original graph. 
+
+    If weights are not used (`useWeights = False`), the resulting
+    network will be undirected and the weight of each new edge will be
+    1/(k_i-1), where k_i is the degree of the common node in `net`.
+
+    If weights are used (`useWeights = True`), the resulting network
+    will be directed and the weight of edge (e_ij, e_jk) will be
+    w_jk/sum_{x != i} w_jx, where the indices i, j and k refer to
+    nodes in `net`.
+
+    Parameters
+    ----------
+    net : pynet.SymmNet object
+        The original graph that is used for constructing the line
+        graph.
+    useWeights : boolean
+        If True, the edge weights will be used when constructing the
+        line graph.
+    output : file object
+        If given, the edges will be written to output in edg-format
+        instead of returning a pynet.Net() or pynet.SymmNet() object.
+    format : str, 'edg' or 'net'
+        If `output` is specified, `format` specifies how the output is
+        written. 'edg' is the standard edge format (FROM TO WEIGHT)
+        and 'net' gives the Pajek format.
+
+    Return
+    ------
+    IF `output` is None:
+        linegraph : pynet.SymmNet or pynet.Net object
+            The weighted line graph.
+    id_array : numpy.array with shape (len(net.edges), 2)
+        Array for converting the nodes in the line graph back into the
+        edges of the original graph. id_array[EDGE_ID] contains the
+        two end nodes of given edge, where EDGE_ID is the same as used
+        in `linegraph`.
+    """
+
+    if output is None:
+        if useWeights:
+            linegraph = pynet.Net()
+        else:
+            linegraph = pynet.SymmNet()
+    edge_map = dict() # edge_map[sorted([n_i, n_j])] = new_node_ID
+
+    if output is not None and format == 'net':
+        # Print Pajek file header.
+        N_edges = len(list(net.edges))
+        output.write("*Vertices %d\n" % N_edges)
+        for i in range(N_edges):
+            output.write('%d "%d"\n' % (i, i))
+        N_edge_links = 0
+        for n in net:
+            degree = len(list(net[n]))
+            N_edge_links += (degree*(degree-1))/2
+        if useWeights:
+            output.write("*Arcs %d\n" % (2*N_edge_links,))
+        else:
+            output.write("*Edges %d\n" % N_edge_links)
+
+    # Go through all nodes (n_c = center node), and for each node, go
+    # through all pairs of neighbours (n_i and n_j). The edges
+    # e_i=(n_c,n_i) and e_j=(n_c,n_j) are nodes in the line graph, so
+    # we add a link between them.
+    for n_c in net:
+        strength = net[n_c].strength()
+        nb = list(net[n_c]) # List of neighbours
+        for i, n_i in enumerate(nb):
+            e_i = edge_map.setdefault(tuple(sorted([n_c,n_i])), len(edge_map))
+            other_nb = (nb[:i]+nb[i+1:] if useWeights else nb[i+1:])
+            for n_j in other_nb:
+                e_j = edge_map.setdefault(tuple(sorted([n_c,n_j])), len(edge_map))
+                if useWeights:
+                    w = net[n_c][n_j]/(strength - net[n_c][n_i])
+                else:
+                    w = 1.0/(len(nb)-1)
+                if output is None:
+                    linegraph[e_i][e_j] = w
+                else:
+                    output.write(" ".join(map(str, [e_i, e_j, w])) + "\n")
+
+    # Construct id_array from edge_map
+    id_array = np.zeros((len(edge_map), 2), int)
+    for node_pair, edgeID in edge_map.iteritems():
+        id_array[edgeID] = list(node_pair)
+
+    if output is None:
+        return linegraph, id_array
+    else:
+        return id_array
+
+
 def netConfiguration(net,keepsOrigNet=False,seed=0):
+    """
+    netConfiguration:
+    This function generates the configuration network of an incoming arbitrary net. 
+    It keeps the degree of each node but randomize the edges between them.
+		
+    Incoming parameters:
+        net
+        keepsOrigNet(=False) - optional
+        seed - optional
+    Outgoing parameters:
+        configuration_net - the shuffled network
 	"""
-	netConfiguration:
-		This function generates the configuration network of an incoming arbitrary net. 
-		It keeps the degree of each node but randomize the edges between them.
-		
-		Incoming parameters:
-			net
-			keepsOrigNet(=False) - optional
-			seed - optional
-		Outgoing parameters:
-			configuration_net - the shuffled network
-	"""
-	if seed!=0:
-		random.seed(int(seed))
+    if seed!=0:
+        random.seed(int(seed))
 
-	newNet=pynet.SymmNet()
-	if keepsOrigNet:
-		testNet=pynet.SymmNet()
-		for edge in net.edges:
-			testNet[edge[0],edge[1]]=edge[2]	
-	else:
-		testNet=net
-	edgeList=[]
-		
-	for edge in net.edges:
-		edgeList.append(edge)
-		
-	firstEdgeID=0
-	while firstEdgeID !=(len(edgeList)):
-		
-		secondEdgeID=firstEdgeID
-		
-		while secondEdgeID==firstEdgeID:
-			secondEdgeID=random.randint(0,len(edgeList)-1)
-		
-		if (edgeList[firstEdgeID][1]==edgeList[secondEdgeID][0]) or (edgeList[secondEdgeID][1]==edgeList[firstEdgeID][0]):
-			continue
-		if (edgeList[firstEdgeID][1]==edgeList[secondEdgeID][0]) and (edgeList[secondEdgeID][1]==edgeList[firstEdgeID][0]):
-			continue
-		if (edgeList[firstEdgeID][0]==edgeList[secondEdgeID][0]) or (edgeList[firstEdgeID][1]==edgeList[secondEdgeID][1]):
-			continue
-		if (newNet[edgeList[firstEdgeID][0],edgeList[secondEdgeID][1]]>0.0) or (newNet[edgeList[secondEdgeID][0],edgeList[firstEdgeID][1]]>0.0):
-			continue
-		if (testNet[edgeList[firstEdgeID][0],edgeList[secondEdgeID][1]]>0.0) or (testNet[edgeList[secondEdgeID][0],edgeList[firstEdgeID][1]]>0.0):
-			continue
-		
-		edgeList[firstEdgeID][1]+=edgeList[secondEdgeID][1]
-		edgeList[secondEdgeID][1]=edgeList[firstEdgeID][1]-edgeList[secondEdgeID][1]
-		edgeList[firstEdgeID][1]=edgeList[firstEdgeID][1]-edgeList[secondEdgeID][1]
-		
-		newNet[edgeList[firstEdgeID][0],edgeList[secondEdgeID][1]]=0.0
-		newNet[edgeList[secondEdgeID][0],edgeList[firstEdgeID][1]]=0.0
+    newNet=pynet.SymmNet()
+    if keepsOrigNet:
+        testNet=pynet.SymmNet()
+        for edge in net.edges:
+            testNet[edge[0],edge[1]]=edge[2]	
+    else:
+        testNet=net
+    edgeList=[]
 
-		testNet[edgeList[firstEdgeID][0],edgeList[secondEdgeID][1]]=0.0
-		testNet[edgeList[secondEdgeID][0],edgeList[firstEdgeID][1]]=0.0
-	
-		newNet[edgeList[firstEdgeID][0],edgeList[firstEdgeID][1]]=1.0
-		newNet[edgeList[secondEdgeID][0],edgeList[secondEdgeID][1]]=1.0
-		testNet[edgeList[firstEdgeID][0],edgeList[firstEdgeID][1]]=1.0
-		testNet[edgeList[secondEdgeID][0],edgeList[secondEdgeID][1]]=1.0
-		
-		firstEdgeID+=1
-		
-	return newNet
+    for edge in net.edges:
+        edgeList.append(edge)
 
-    
+    for i in range(len(edgeList)):
+
+        j=i
+        while j==i:
+            j=random.randint(0,len(edgeList)-1)
+
+        if ((edgeList[i][1]==edgeList[j][0]) 
+            or (edgeList[j][1]==edgeList[i][0])):
+            continue
+        if ((edgeList[i][1]==edgeList[j][0]) 
+            and (edgeList[j][1]==edgeList[i][0])):
+            continue
+        if ((edgeList[i][0]==edgeList[j][0]) 
+            or (edgeList[i][1]==edgeList[j][1])):
+            continue
+        if ((newNet[edgeList[i][0],edgeList[j][1]]>0.0) 
+            or (newNet[edgeList[j][0],edgeList[i][1]]>0.0)):
+            continue
+        if ((testNet[edgeList[i][0],edgeList[j][1]]>0.0) 
+            or (testNet[edgeList[j][0],edgeList[i][1]]>0.0)):
+            continue
+
+        edgeList[i][1]+=edgeList[j][1]
+        edgeList[j][1]=edgeList[i][1]-edgeList[j][1]
+        edgeList[i][1]=edgeList[i][1]-edgeList[j][1]
+
+        newNet[edgeList[i][0],edgeList[j][1]]=0.0
+        newNet[edgeList[j][0],edgeList[i][1]]=0.0
+
+        testNet[edgeList[i][0],edgeList[j][1]]=0.0
+        testNet[edgeList[j][0],edgeList[i][1]]=0.0
+
+        newNet[edgeList[i][0],edgeList[i][1]]=1.0
+        newNet[edgeList[j][0],edgeList[j][1]]=1.0
+        testNet[edgeList[i][0],edgeList[i][1]]=1.0
+        testNet[edgeList[j][0],edgeList[j][1]]=1.0
+
+
+    return newNet
+
 if __name__ == '__main__':
     """Run unit tests if called."""
     from tests.test_transforms import *
