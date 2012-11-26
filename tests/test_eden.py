541ebfe06d5145e26d8ce944bba2bcb728f1c059
mtkivela <mtkivela>
2012-11-26 16:25:48 +0000

    Added tests for FST distance in msat data.

--- tests/test_eden.py
+++ tests/test_eden.py
@@ -36,6 +36,7 @@ class TestEden(unittest.TestCase):
 		self.abundance_data1=["0 0 1",
 				     "0 0 2",
 				     "1 1 1"]
+		self.msat_data_filename=os.path.join("testData","msat_testdata.txt")
 
 	def test_distances_abundances(self):
 		data1=eden.MicrosatelliteDataHaploid(self.abundance_data1)
@@ -54,7 +55,15 @@ class TestEden(unittest.TestCase):
 		af2.init_msData(ms1,[[0,2],[1,3]])
 		assert af2.freqsTable[0]==[{'A':2},{'A':2},{'B':1},{'A':1,'B':1},{'B':1,'A':1},{'C':2}]
 		assert af2.freqsTable[1]==[{'A':2},{'A':2},{'B':1},{'A':1,'B':1},{'B':1,'A':1},{'C':2}]
-		print af2.getFST()[0,1]
+		#print af2.getFST()[0,1]
+
+	def test_distances_allele_freqs_msat(self):
+		af=eden.AlleleFrequencyTable()
+		af.init_freqFile(self.msat_data_filename)
+		d=af.getFST()
+		self.assertEqual(round(d["CAR","CIN"],3),0.075)
+		self.assertEqual(round(d["CAM","CIN"],3),0.025)
+		self.assertEqual(round(d["CAR","CAM"],3),0.088)
 
 
 	def test_distances_binary_data(self):
@@ -171,6 +180,7 @@ if __name__ == '__main__':
 		suite = unittest.TestSuite()
 		suite.addTest(TestEden("test_distances_abundances"))
 		suite.addTest(TestEden("test_distances_allele_freqs"))
+		suite.addTest(TestEden("test_distances_allele_freqs_msat"))
 		suite.addTest(TestEden("test_distances_binary_data"))
 		suite.addTest(TestEden("test_distances_individuals"))
 		suite.addTest(TestEden("test_distances_populations"))
